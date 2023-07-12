a29c4546ec0599ff74aca353eaef7e827a687d01
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-07-12 11:58:42 +0100

    simple postcode input works

--- src/PersonalDetails/PersonalDetails.tsx
+++ src/PersonalDetails/PersonalDetails.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { useState } from "react"
+import React from "react"
 
-const PersonalDetails = () => {
-    const [postcode, setPostcode] = useState("")
-    const handlePostcode = (event) => {
+const PersonalDetails = (): JSX.Element => {
+    const [postcode, setPostcode] = useState<string>("")
+    const handlePostcode = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPostcode(event.target.value)
+        console.log(postcode)
     }
 
     return (
         <div>
-            <input type="text" id="postcode" value={postcode} placeholder="Enter your postcode"  onChange={event => handlePostcode(event)} className="text-black rounded"/>
+            <input className="text-black rounded" type="text" id="postcode" value={postcode} placeholder="Enter your postcode"  onChange={(event: React.ChangeEvent<HTMLInputElement>) => handlePostcode(event)}/>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       GetOn
+      {/* <input className="text-black rounded" type="text" id="test"/> */}
       <PersonalDetails/> 
     </main>
   )
