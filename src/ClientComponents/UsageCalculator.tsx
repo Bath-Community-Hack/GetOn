0caee704677d59411029130db29aa38d2d9ddb2c
Adam Jacobs <adamjacobs173@googlemail.com>
2023-07-13 20:15:49 +0100

    Working usage calculator

--- src/ClientComponents/UsageCalculator.tsx
+++ src/ClientComponents/UsageCalculator.tsx
@@ -1,12 +1,15 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Image from "next/image"
+
+import useDerivedState from '../useDerivedState'
 
 import fullPerson from '../../public/Get On-04.png'
 import emptyPerson from '../../public/Get On-05.png'
 
 interface UsageCalculatorProps {
-    setUsage: Function
+    usage: number
+    setUsage: (mbps: number) => void
 }
 
 function PeopleSelector({people, setPeople}:{
@@ -33,17 +36,54 @@ function PeopleSelector({people, setPeople}:{
     </div>
 }
 
+const coefficients = {
+    streaming: 4.5,
+    social: 2,
+    work: 5,
+    gaming: 6
+}
+
 const UsageCalculator = (props: UsageCalculatorProps) => {
+
     const [total, setTotal] = useState(0)
-    const [streaming, setStreaming] = useState(0)
-    const [social, setSocial] = useState(0)
-    const [work, setWork] = useState(0)
-    const [gaming, setGaming] = useState(0)
+    const [people, setPeople] = useState<Record<string,number>>({
+        streaming: 0,
+        social: 0,
+        work: 0,
+        gaming: 0
+    })
+    const [streaming, setStreaming] = useDerivedState(
+        [people, setPeople], ['streaming'])
+    const [social, setSocial] = useDerivedState(
+        [people, setPeople], ['social'])
+    const [work, setWork] = useDerivedState(
+        [people, setPeople], ['work'])
+    const [gaming, setGaming] = useDerivedState(
+        [people, setPeople], ['gaming'])
 
     function setTotalConstrained(n:number) {
         const max = Math.max(streaming, social, work, gaming)
         setTotal(Math.max(n,max))
     }
+
+    function calculateUsage(
+        people: Record<string,number>, total:number
+    ) {
+        const orderedCoeffs = Object.entries(coefficients)
+        orderedCoeffs.sort(([_,a],[__,b]) => b-a)
+        let usage = 0
+        let remaining = total
+        for (const [key,coeff] of orderedCoeffs) {
+            const num = Math.min(remaining,people[key])
+            usage += num * coeff
+            remaining -= num
+        }
+        return usage
+    }
+
+    useEffect(() => {
+        props.setUsage(calculateUsage(people, total))
+    }, [people, total])
 
     function setAndSyncTotal(f: (n:number) => void) {
         return (n:number) => {
@@ -51,7 +91,6 @@ const UsageCalculator = (props: UsageCalculatorProps) => {
                 setTotal(n)
             }
             f(n)
-            // FIXME update usage
         }
     }
 
@@ -70,6 +109,20 @@ const UsageCalculator = (props: UsageCalculatorProps) => {
             <PeopleSelector people={work} setPeople={setAndSyncTotal(setWork)}/>
             <p>Gaming</p>
             <PeopleSelector people={gaming} setPeople={setAndSyncTotal(setGaming)}/>
+
+            <div className="w-full mt-3 border-b border-[#25A]"/>
+            <div className="text-center">
+                <div className="font-extrabold text-[#25A] text-xl">
+                    Your peak usage:
+                </div>
+                <div className="flex items-center justify-center mt-2">
+                    <span className="font-extrabold text-[#25A] text-5xl">
+                        {props.usage}
+                    </span>
+                    <div className="w-3" />
+                    <span className="font-bold">Megabytes <p>/ second</p></span>
+                </div>
+            </div>
         </div>
     )
 
--- src/app/usage/page.tsx
+++ src/app/usage/page.tsx
@@ -3,14 +3,16 @@
 import UsageCalculator from "@/ClientComponents/UsageCalculator";
 import QuizTemplate from "@/ServerComponents/QuizTemplate";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Usage() {
   const router = useRouter()
 
+  const [usage, setUsage] = useState(0)
+
   return <QuizTemplate>
     <div className="mt-2 w-full flex flex-col items-center">
-      <UsageCalculator setUsage={(x:any)=>{}} />
-
+      <UsageCalculator {...{usage, setUsage}} />
       <button className="h-16 text-lg text-blue-800 underline hover:text-blue-400 cursor-pointer"
               onClick={()=>router.push('/results')}
       >
