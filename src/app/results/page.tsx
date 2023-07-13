9031f690b4ae4fa4ba3bed628b48df2a106900a7
Adam Jacobs <adamjacobs173@googlemail.com>
2023-07-14 00:07:00 +0100

    results page back to centre

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -6,9 +6,9 @@ import localFont from 'next/font/local'
 const gotham = localFont({src: '../../../public/fonts/Gotham-Font/GothamMedium.ttf'})
 
 export default function Results() {
-  return <main className={gotham.className + "flex min-h-screen flex-col items-center justify-start" }>
+  return <main className={gotham.className + " flex min-h-screen flex-col items-center justify-start" }>
     <Image className="max-w-[400px] w-full" src={results} alt="yes, the whole page is a screenshot. woops" />
-    <div className="text-center mt-3">
+    <div className="text-center mt-3 max-w-[400px]">
     <a className="text-[#1C75BC] hover:text-blue-400"
        href="/apitest">
       (We ran out of time... Click this link for a demo of the package search functionality)
