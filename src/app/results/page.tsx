4b35f96f5a489294250d3a67f4928f0524c0d40d
Adam Jacobs <adamjacobs173@googlemail.com>
2023-07-14 15:17:28 +0100

    Remove link to compare the market data as it's a legal liability

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -9,10 +9,7 @@ export default function Results() {
   return <main className={gotham.className + " flex min-h-screen flex-col items-center justify-start" }>
     <Image className="max-w-[400px] w-full" src={results} alt="yes, the whole page is a screenshot. woops" />
     <div className="text-center mt-3 max-w-[400px]">
-    <a className="text-[#1C75BC] hover:text-blue-400"
-       href="/apitest">
-      (We ran out of time... Click this link for a demo of the package search functionality)
-    </a>
+      (We ran out of time... real results coming soon)
     </div>
   </main>
 }
